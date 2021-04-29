7ec3e45f911ce85ed60a2f087d7f7d30a3b710ee
ufonumo <ufonumo@gmail.com>
2021-04-29 03:13:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import HeroSection from './components/HeroSection';
 import {BrowserRouter , Route} from "react-router-dom";
 import Contact from './pages/Contact';
 import Products from './pages/Products';
+import React, { useEffect, useState } from 'react';
+import Loader from "react-loader-spinner";
 
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  }, [])
+
   return (
     <BrowserRouter>
-
-      <div className="App">
-        <Header/>
-        <Route exact path='/' component={HeroSection} />
-        <Route exact path='/Contact' component={Contact} />
-        <Route exact path='/Products' component={Products} />
-
-      </div>
+        <div className="App">
+          {
+            loading ?
+              <Loader
+              type="Puffs"
+              color="#ff6900"
+              height={100}
+              width={100}
+              marginTop='100px'
+              className='ewa_loader'
+            />
+          :
+            <>
+              <Header/>
+              <Route exact path='/' component={HeroSection} />
+              <Route exact path='/Contact' component={Contact} />
+              <Route exact path='/Products' component={Products} />
+            </>
+          }
+        </div>
+      
     </BrowserRouter>
 
   );
